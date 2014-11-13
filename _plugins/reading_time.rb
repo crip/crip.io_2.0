9c1bc1a6deae1744bae3cbadf02ddaf14a138d9c
Johnie Hjelm <johnie.hjelm@symbio.com>
2014-11-13 20:56:28 +0100

    Minor updates

--- _plugins/reading_time.rb
+++ _plugins/reading_time.rb
@@ -10,8 +10,8 @@ module ReadingTimeFilter
 
 		words = input.split.size;
 		minutes = ( words / words_per_minute ).floor
-		minutes_label = minutes === 1 ? " minute" : " minutes"
-		minutes > 0 ? "about #{minutes} #{minutes_label}" : "less than 1 minute"
+		minutes_label = minutes === 1 ? " min" : " min"
+		minutes > 0 ? "about #{minutes} #{minutes_label}" : "~ 1 min"
 	end
 end
 
